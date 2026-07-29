a5cd5ff4b37e779fe8699c4ec3c4df3c16267b29
salini4kamal <salini4kaml@gmail.com>
2026-07-29 13:51:32 +0530

    Remove unwanted copy files

--- Ecommerce website/backend copy/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB=()=>{
-    mongoose.connect(process.env.DB_URI).then((data)=>{
-    console.log("MongoDB connected with server: ", data.connection.host);
-}).catch((err)=>{
-    console.log("Error connecting to MongoDB",err);
-})
-} 
